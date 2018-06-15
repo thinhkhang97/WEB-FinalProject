8a4413af68e464b4c71a3b83a29edd52f954e23f
DESKTOP-3OT6EDP\User_Pro <linhnguyen1512286@gmail.com>
2018-06-16 00:54:41 +0700

    login with db

--- Selling-Camera/controller/homeController.js
+++ Selling-Camera/controller/homeController.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/',(req,res)=>{
-    res.render('home/index');
+    res.render('home/index', {
+        login: true
+    });
 });
 
 module.exports = router;
--- Selling-Camera/controller/loginController.js
+++ Selling-Camera/controller/loginController.js
@@ -1,8 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var SHA256 = require('crypto-js/sha256');
+var db=require('../fn/db');
 
 router.get('/',(req,res)=>{
     res.render('login/index');
 });
+router.post('/', (req, res) => {
+    var user = {
+        username: req.body.username,
+        password: SHA256(req.body.rawPWD).toString()
+    };
 
+    var sql = `select * from users where username = '${user.username}' and passwords = '${user.password}'`;
+    var rows = db.load(sql);
+
+    if (rows.length > 0) {
+        req.session.isLogged = true;
+        req.session.user = rows[0];
+        req.session.cart = [];
+
+        var url = '/';
+        if (req.query.retUrl) {
+            url = req.query.retUrl;
+        }
+        res.redirect(url);
+
+    } else {
+        var vm = {
+            showError: true,
+            errorMsg: 'Login failed'
+        };
+        res.render('home/index', {
+            login: false
+        });
+    }
+});
 module.exports = router;
