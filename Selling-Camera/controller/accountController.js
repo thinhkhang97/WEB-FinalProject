7b37854c052e89608c11d5dbe51d9e3f20c3fc05
1512144 <nhathai74@gmail.com>
2018-06-26 17:35:41 +0700

    + Sửa giao diện, hoàn thiện chức năng cập nhật thông tin

--- Selling-Camera/controller/accountController.js
+++ Selling-Camera/controller/accountController.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var accountRepo = require('../repository/accountRepo');
 var router = express.Router();
+var SHA256 = require('crypto-js/sha256');
+var moment = require('moment');
 
 router.get('/', (req, res) => {
     res.redirect('account/profile');
@@ -11,14 +13,49 @@ router.get('/profile', (req, res) => {
     accountRepo.loadAccount(req.session.users.id).then(rows => {
         var vm = {
             userdetail: rows[0],
+            layout: 'profileLayout.handlebars',
+
             //user: req.session.user
         };
+        console.log(vm.userdetail);
+
+        vm.userdetail.ngaysinh = moment(vm.userdetail.ngaysinh).format('YYYY-MM-DD');
         res.render('profile/index', vm);
     });
 });
 router.post('/profile', (req, res) => {
-    accountRepo.update(req.body).then(value => {
-        res.redirect('/profile');
+    var dob = moment(req.body.dob).format('YYYY-MM-DDTHH:mm');
+
+    console.log(req.body.dob);
+    console.log(dob);
+    console.log(req.body);
+    if (req.body.new_password != '') {
+        var user = {
+            ID: req.session.users.id,
+            passwords: SHA256(req.body.new_password).toString(),
+            hoten: req.body.name,
+            gioitinh: req.body.gender,
+            ngaysinh: dob,
+            dthoai: req.body.phone_number,
+            diachi: req.body.address,
+            //permission: 0
+        };
+    } else {
+        var user = {
+            ID: req.session.users.id,
+            passwords: req.session.users.passwords,
+            hoten: req.body.name,
+            gioitinh: req.body.gender,
+            ngaysinh: dob,
+            dthoai: req.body.phone_number,
+            diachi: req.body.address,
+            //permission: 0
+        };
+    }
+    console.log(user);
+    console.log(req.headers.referer);
+    accountRepo.update(user).then(value => {
+        res.redirect(req.headers.referer);
     });
 });
 module.exports = router;
--- users.sql
+++ users.sql
@@ -2,10 +2,13 @@ use camera;
 create table users(
 ID int not null auto_increment,
 hoten nvarchar(40) not null,
+gioitinh nvarchar(3),
 dthoai char(11) not null,
+ngaysinh date,
 diachi nvarchar(100) not null,
 email nvarchar(30),
 username nvarchar(30) not null,
-passwords nvarchar(32) not null, 
+passwords nvarchar(1000) not null, 
+email nvarchar(30),
 primary key(ID)
 );
