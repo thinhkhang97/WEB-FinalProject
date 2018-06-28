0a6c05d946e0a3630b18bbf236a68634f63108c0
1512144 <nhathai74@gmail.com>
2018-06-29 04:53:39 +0700

    + Load đơn hàng, chỉnh sửa đơn hàng, đặt hàng(còn chưa lưu được danh sách sản phẩm vì lỗi lấy ID đơn hàng)

--- Selling-Camera/controller/cartController.js
+++ Selling-Camera/controller/cartController.js
@@ -2,11 +2,11 @@ var express = require('express');
 var cartRepo = require('../repository/cartRepo');
 var productRepo = require('../repository/productRepo');
 var getF = require('../fn/getFolder');
+var accountRepo = require('../repository/accountRepo');
+var moment = require('moment');
 
 var router = express.Router();
-
 router.get('/', (req, res) => {
-
     var arr = [];
     req.session.cart=[{proID:'1', proQuantity:1}, {proID:'2', proQuantity:2}, {proID:'3', proQuantity:2}];
     console.log(req.session.cart)
@@ -44,9 +44,49 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/pay', (req, res) => {
+
+    var arr = [];
+    req.session.cart=[{proID:'1', proQuantity:1}, {proID:'2', proQuantity:2}, {proID:'3', proQuantity:2}];
+    for (var i = 0; i < req.session.cart.length; i++) {
+        var cartItem = req.session.cart[i];
+        var p = productRepo.loadProductByID(cartItem.proID);
+        arr.push(p);
+    }
+    var user = accountRepo.loadAccount(req.session.users.id);
+    arr.push(user);
+    var items = [];
+    var toTal = 0, ship = 0, toTal_ship = 0;
+    Promise.all(arr).then(result => {
+        for (var i = result.length - 2; i >= 0; i--) {
+            var pro = result[i][0];
+            var fd = getF.getCatgoryById(pro.proCatID);
+            var item = {
+                Product: pro,
+                fd: fd,
+                Quantity: req.session.cart[i].proQuantity,
+                Amount: pro.proPrice * req.session.cart[i].proQuantity
+            };
+            toTal += pro.proPrice * req.session.cart[i].proQuantity;
+            items.push(item);
+        }
+        u = result[result.length - 1][0];
+        
+        ship=50000;
+        toTal_ship= toTal + ship;
+        var vm = {
+            items: items,
+            total: toTal,
+            ship: ship,
+            total_ship: toTal_ship,
+            user: u,
+            layout: 'profileLayout.handlebars',
+        };
+        res.render('cart/pay', vm);
+    });
+});
+
 router.post('/add', (req, res) => {
-    req.session.cart.push({proID: 2, proQuantity:1});
-    console.log(req.session.cart)
     var item = {
         proID: req.body.proID,
         proQuantity: +req.body.proQuantity
@@ -57,7 +97,6 @@ router.post('/add', (req, res) => {
 });
 
 router.post('/remove', (req, res) => {
-    console.log(req.session.cart)
     cartRepo.remove(req.session.cart, req.body.proID_R);
     res.redirect(req.headers.referer);
 });
@@ -70,5 +109,53 @@ router.post('/change', (req, res) => {
     };
     cartRepo.change(req.session.cart, item);
     res.redirect(req.headers.referer);
+});
+
+router.post('/pay', (req, res) => {
+    var arr = [];
+    req.session.cart=[{proID:'1', proQuantity:1}, {proID:'2', proQuantity:2}, {proID:'3', proQuantity:2}];
+    for (var i = 0; i < req.session.cart.length; i++) {
+        var cartItem = req.session.cart[i];
+        var p = productRepo.loadProductByID(cartItem.proID);
+        arr.push(p);
+    }
+    var toTal = 0, ship = 0, toTal_ship = 0;
+    Promise.all(arr).then(result => {
+        for (var i = result.length - 1; i >= 0; i--) {
+            var pro = result[i][0];
+            toTal += pro.proPrice * req.session.cart[i].proQuantity;
+        }       
+        ship=50000;
+        toTal_ship= toTal + ship;
+        var date = moment(new Date()).format('YYYY-MM-DDTHH:mm');
+        var order={
+            OrderDate: date,
+            UserID: req.session.users.id,
+            Ship: ship,
+            Total: toTal_ship,
+        }
+        cartRepo.insertOrder(order);
+        //var oid = cartRepo.insertOrder(order);
+       // result.push(oid);
+        //console.log(oid);
+        return result;
+    }).then(result => {
+        console.log(result);
+        for (var i = result.length - 1; i >= 0; i--) {
+            var pro = result[i][0];
+            var item = {
+                OrderID: 1,
+                ProID: pro.proID,
+                Price: pro.Price,
+                Quantity: req.session.cart[i].proQuantity,
+                Amount: pro.proPrice * req.session.cart[i].proQuantity
+            };
+            cartRepo.insertOrderDetail(item);
+        }
+        req.session.cart=[];
+        return null;
+    });
+    console.log(req.session.cart);
+    res.redirect('/');
 });
 module.exports = router;
