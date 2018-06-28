b7bfa98d858c6035f127d9f4219e4225fe2e0377
DESKTOP-3OT6EDP\User_Pro <linhnguyen1512286@gmail.com>
2018-06-28 16:39:57 +0700

    Admin-Add/Delete Manufactures

--- Selling-Camera/repository/manufactureRepo.js
+++ Selling-Camera/repository/manufactureRepo.js
@@ -11,4 +11,12 @@ exports.loadProductByManuID = ID =>{
 exports.getAllManufactures = () =>{
     return db.load(`select * from manufacture`);
 }
-
+exports.getAManufactures=ID=>{
+    return db.load(`select * from manufacture where manID=${ID}`);
+}
+exports.insertNewManufactures = c =>{
+    return db.save(`insert into manufacture (manName,manCountry,manInfo) values ('${c.manName}','${c.manCountry}','${c.manInfo}')`);
+}
+exports.deleteManufacetures = c=>{
+    return db.save(`delete from manufacture where manID = ${c.manID}`);
+}
