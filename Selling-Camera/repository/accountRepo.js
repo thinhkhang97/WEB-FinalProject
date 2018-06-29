d1545ed0fbdac5f17c7e2a64f8ca7692dc6e137b
ANNGUYEN-PC\An Nguyen <annguyen9682@gmail.com>
2018-06-29 09:51:39 +0700

    update UI

--- Selling-Camera/repository/accountRepo.js
+++ Selling-Camera/repository/accountRepo.js
@@ -8,13 +8,12 @@ exports.update = (c) =>{
     var sql = `update users set hoten = '${c.hoten}', gioitinh = '${c.gioitinh}', dthoai = '${c.dthoai}', ngaysinh = '${c.ngaysinh}', diachi = '${c.diachi}', passwords = '${c.passwords}' where ID = ${c.ID}`;
     return db.save(sql);
 }
-
-exports.loadOrder = (id) => {
-    var sql = `select * from orders where UserID='${id}'`;
-    return db.load(sql);
+exports.loadAllAccount = ()=>{
+    return db.load(`select * from users`);
 }
-
-exports.loadOrderDetail = (id) => {
-    var sql = `select * from orderdetails where OderID='${id}'`;
-    return db.load(sql);
+exports.deleteUser= c=>{
+    return db.save(`delete from users where ID = ${c.ID}`);
+}
+exports.updateUser = c=>{
+    return db.save(`update users set hoten='${c.hoten}',dthoai='${c.dthoai}',diachi='${c.diachi}',email='${c.email}',username='${c.username}' where ID=${c.ID}`);
 }
